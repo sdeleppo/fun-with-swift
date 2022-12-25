b29ff42d45fec0fcc51be52212d443d127961591
sdeleppo <sdeleppo@yahoo.com>
2022-12-24 19:09:06 -0500

    Base project commit

---
+++ RandomPhoto/RandomPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhoto
+//
+//  Created by Sarah Deleppo on 12/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
